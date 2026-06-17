e14d12a5998e83150c7681208718d4ca7534a6bb
Anuj Basnet <anujbasnet14@gmail.com>
2026-06-17 13:35:22 -0400

    Added word array for the story choice

---
+++ script.js
@@ -0,0 +1,48 @@
+// Declare arrays for the five parts of the sentence.
+var partOneWords = [
+  "The turkey",
+  "Mom",
+  "Dad",
+  "The dog",
+  "My teacher",
+  "The elephant",
+  "The cat"
+];
+
+var partTwoWords = [
+  "sat on",
+  "ate",
+  "danced with",
+  "saw",
+  "doesn't like",
+  "kissed"
+];
+
+var partThreeWords = [
+  "a funny",
+  "a scary",
+  "a goofy",
+  "a slimy",
+  "a barking",
+  "a fat"
+];
+
+var partFourWords = [
+  "goat",
+  "monkey",
+  "fish",
+  "cow",
+  "frog",
+  "bug",
+  "worm"
+];
+
+var partFiveWords = [
+  "on the moon",
+  "on the chair",
+  "in my spaghetti",
+  "in my soup",
+  "on the grass",
+  "in my shoes"
+];
+
